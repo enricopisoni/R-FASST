5fa5d014d4f5f90cb9319970db941c257831ae58
alessandro.mascherpa <alessandro.mascherpa@piksel.com>
2020-03-27 17:34:51 +0100

    risk-rate: indent with tabs

--- rrate.R
+++ rrate.R
@@ -3,7 +3,7 @@
 # calculates with an analytical function the Risk Rate (RR) for a given PM exposure;
 #
 # Input parameters:
-#   - C  - cause of death currently evaluated;
+#   - C	 - cause of death currently evaluated;
 #   - PM - PM concentration (can be an array);
 #
 # Output parameters:
@@ -15,19 +15,18 @@
 #
 rrate <- function( C, PM )
 {
+	# Initialize RR with same dimensions as PM
+	RR <- numeric( length( PM ) )
 
-        # Initialize RR with same dimensions as PM
-        RR <- numeric( length( PM ) )
+	# elements less than than C[4]
+	jj <- PM >= C[ 4 ]
+	# Other grid cells: RR as a function of 4
+	# C[i] function parameter
+	RR[ jj ] <- 1 + C[1] * ( 1 - exp( -C[2] * ( PM[ jj ] - C[4] ) ^ C[3] ) )
 
-        # elements less than than C[4]
-        jj <- PM >= C[ 4 ]
-        # Other grid cells: RR as a function of 4
-        # C[i] function parameter
-        RR[ jj ] <- 1 + C[1] * ( 1 - exp( -C[2] * ( PM[ jj ] - C[4] ) ^ C[3] ) )
+	# grid cells where PM < C[4] get RR=1
+	RR[ ! jj ]  <- 1
 
-        # grid cells where PM < C[4] get RR=1
-        RR[ ! jj ]  <- 1
-
-        # return result
-        RR
+	# return result
+	RR
 }
