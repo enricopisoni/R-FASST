27304810fcea10380c8dcdfc8a3f83e5a937e715
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-02 18:03:20 +0200

    risk-rate: input parameters added

--- health-impacts.R
+++ health-impacts.R
@@ -2,10 +2,14 @@
 
 #' Health impacts from high resolution FASST grid maps;
 #'
-#' @param project    project name;
-#' @param dir.config named list with paths configuration;
-#'                   the list should define fields listed
-#'                   in file: fasst-config.R;
+#' @param project       project name;
+#' @param model         model name;
+#' @param version       current version;
+#' @param dir.root.out  output directory root (absolute path or
+#'                      relative to working directory);
+#' @param dir.config    named list with paths configuration;
+#'                      the list should define fields listed
+#'                      in file: fasst-config.R;
 #'
 #' Side effects:
 #'   - ...
@@ -13,6 +17,9 @@
 
 health:impact <- function(
                    project,
+                   model,
+                   version,
+                   dir.root.out,
                    dir.config
                  )
 {
