b03dc6737e0635c213c0256181c36d02ecd3de66
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-09 11:10:00 +0200

    risk-rate: configuration updated

--- health-impacts.R
+++ health-impacts.R
@@ -45,7 +45,7 @@ health.impact <- function(
 
 
     #    -- line 72 - block 2
-
+    print( config.internal$L1 )
 
 
     # internal configuration - do not modify block - end
@@ -66,58 +66,58 @@ health.impact <- function(
 config.internal <- list(
 
       # available years with ssp populatation. if needed for scenarios, interpolate between those years
-      ssp_yrs <- c( '2000','2010','2020','2030','2040','2050','2060','2070','2080','2090','2100' ),
+      ssp_yrs = c( '2000','2010','2020','2030','2040','2050','2060','2070','2080','2090','2100' ),
 
       # counterfactual level for SDMA8h for O3 health impact - GBD2017 uses between 29.1 and 35.7 for M3M. Malley/Turner use between 26.7 and 31.1
-      SDM8THR <- 29.1,
+      SDM8THR = 29.1,
       # counterfactual level for ADMA8h for O3 health impact - Turner uses both 26.7 and 31.1
-      ADM8THR <- 26.7,
+      ADM8THR = 26.7,
 
       # risk function parameters
-      rr_param_file <- 'RR_ALL_GBD_2017_FITTINGS_ANALYT.csv',
+      rr_param_file = 'RR_ALL_GBD_2017_FITTINGS_ANALYT.csv',
 
       # AGE CLASSES TO CONSIDER:
-      ages <- seq( from = 0, to = 100, by = 5 ),
+      ages = seq( from = 0, to = 100, by = 5 ),
 
       # ALL AGES FROM MIN TO MAX (MAX 100)
-      agefrac_copd <- c( 0, 100 ),
-      C1           <- ages == agefrac_copd[ 1 ],
-      C2           <- ages == agefrac_copd[ 2 ],
+      agefrac_copd = c( 0, 100 ),
+      C1           = ages == agefrac_copd[ 1 ],
+      C2           = ages == agefrac_copd[ 2 ],
 
       # ALL AGES FROM MIN TO MAX (MAX 100)
-      agefrac_lc   <- c( 0, 100 ),
-      L1           <- ages == agefrac_lc[ 1 ],
-      L2           <- ages == agefrac_lc[ 2 ],
+      agefrac_lc   = c( 0, 100 ),
+      L1           = ages == agefrac_lc[ 1 ],
+      L2           = ages == agefrac_lc[ 2 ],
 
       # ALL AGES FROM MIN TO MAX (MAX 100)
-      agefrac_lri  <- c( 0, 100 ),
-      LR1          <- ages == agefrac_lri[ 1 ],
-      LR2          <- ages == agefrac_lri[ 2 ],
+      agefrac_lri  = c( 0, 100 ),
+      LR1          = ages == agefrac_lri[ 1 ],
+      LR2          = ages == agefrac_lri[ 2 ],
 
       # ALL AGES FROM MIN TO MAX (MAX 100)
-      agefrac_dmt2 <- c( 0, 100 ),
-      DM1          <- ages == agefrac_dmt2[ 1 ],
-      DM2          <- ages == agefrac_dmt2[ 2 ],
+      agefrac_dmt2 = c( 0, 100 ),
+      DM1          = ages == agefrac_dmt2[ 1 ],
+      DM2          = ages == agefrac_dmt2[ 2 ],
 
       # AGE SPECIFIC FROM MIN TO MAX (MAX 95, BECAUSE AGE-SPECIFIC RR AVAILABLE TILL 95)
-      agefrac_ihd  <- c( 25, 95 ),
-      IH1          <- ages == agefrac_ihd[ 1 ],
-      IH2          <- ages == agefrac_ihd[ 2 ],
+      agefrac_ihd  = c( 25, 95 ),
+      IH1          = ages == agefrac_ihd[ 1 ],
+      IH2          = ages == agefrac_ihd[ 2 ],
 
       # OZONE MORTALITIES - NOTE that mortality rates in GBD are expressed as number divided by total population.
       # COPD mortalitiy rates below 25 are zero.
       # ALL AGES FROM MIN TO MAX (MAX 100)
-      agefrac_o3   <- c( 0, 100 ),
-      O1           <- ages == agefrac_o3[ 1 ],
-      O2           <- ages == agefrac_o3[ 2 ],
+      agefrac_o3   = c( 0, 100 ),
+      O1           = ages == agefrac_o3[ 1 ],
+      O2           = ages == agefrac_o3[ 2 ],
 
       # CODs for which RR is aggregated over all age groups
-      COD1         <- c( 'COPD', 'LC', 'LRI', 'DMT2' ),
+      COD1         = c( 'COPD', 'LC', 'LRI', 'DMT2' ),
       # CODs for which RR is age group sepcific
-      COD2         <- c( 'IHD', 'STROKE' ),
+      COD2         = c( 'IHD', 'STROKE' ),
 
       # DO NOT CHANGE!
       # Generates string array of AGE GROUP NAMES (25 TO 95 IN 5 YEAR BINS) 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95
-      AGE_GRP      <- as.character( seq( from = 25, to = 95, by = 5 ) ),
-      nage         <- length( AGE_GRP )
+      AGE_GRP      = as.character( seq( from = 25, to = 95, by = 5 ) ),
+      nage         = length( AGE_GRP )
 )
