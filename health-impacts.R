6cd3e7f04168d13c07410c2d5152716d4e3e7da4
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-10 18:31:48 +0200

    risk-rate: read files with: country codes and country identification gridmap

--- health-impacts.R
+++ health-impacts.R
@@ -17,6 +17,8 @@
 #'                      in file: fasst-config.R;
 #'
 
+library( 'raster' )
+
 health.impact <- function(
                    project,
                    model,
@@ -53,16 +55,37 @@ health.impact <- function(
     # For years > 2040 mortality stats for 2040 are used.
     # For years < 2005, mortality stats for 2005 are used.
 
-    # ;Population country totals
-
-    print( config$files$in.file.pop.country )                           # --remove--
+    # population country totals
     pop <- read.csv( file = config$files$in.file.pop.country )
-    print( head( pop ) )                                                         # --remove--
+
+    # generate dataframe with unique country codes and names from POP file
+    cntr <- unique(
+                subset(
+                        x = pop,
+                        select = c( cntry_id, CNTRY_NAME, ISO )
+                      )
+                  )
+
+    # read country identification gridmap (Ciesin GPW v4)
+    cntrgrid <- raster( config$files$in.file.cntrgrid )
+    print( cntrgrid )                                                       # --remove--
+    latmin <- ymin( cntrgrid )
+    latmax <- ymax( cntrgrid )
+
+    # values of: cntrgrid --> matrice di: 580 righe - 1440 colonne
+    # dati valori della matrice: cambia l'ordine delle righe (quelle in basso vanno in alto e vice versa )
+    # CNTRCODE := matice ( 1440,720 )
+    # LATFUL   := ( ( array con valori in intervallo: 0 |-| 719 ) - 360 / 4.
+    # LONFUL  -- NOT USED!!!!!!
+    # indmin   := indici di LATFUL dove i valori sono == latmin
+    # indmax   := indici di LATFUL dove i valori sono == latmax
+    # CNTRCODE[ 0 : 1439, indmin : indmax-1 ]  = cntrgrid
+    # HRCNTRCODE := CNTRCODE expanded/shrinked to size: 2880,1440
 
 
-
-
-
+    # given an empty matrix( 1440, 720 ) - all values set to 0 - where the indeces 0..719 mean -90..89.75 (steps by .25)
+    # values of cntrgrid are 'overimposed' to empty matrix in order that the cntrgrid latitude, min'n'max, are correct
+    # doulbes the matrix size: from 1440,720 to 2880,1440
 
 
     # prepare output directories
@@ -75,4 +98,3 @@ health.impact <- function(
 }
 
 # ------------------------------------------------------------
-
