9eb36bb27b2fe7807ecfa8bc5c83748b7fd9f02a
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-16 11:59:52 +0200

    risk-rate: removed debug code; lat-lon arraies in medium resolution

--- health-impacts.R
+++ health-impacts.R
@@ -89,24 +89,8 @@ health.impact <- function(
     stroke  <- read.csv( file = config$files$in.file.stroke )
     dmt2    <- read.csv( file = config$files$in.file.dmt2 )
 
-# --remove--  print( config$files$in.file.copd )
-# --remove--      print( head( copd   ))              # --remove--
-# --remove--  print( config$files$in.file.lc )
-# --remove--      print( head( lc     ))
-# --remove--  print( config$files$in.file.lri )
-# --remove--      print( head( lri    ))
-# --remove--  print( config$files$in.file.ihd )
-# --remove--      print( head( ihd    ))
-# --remove--  print( config$files$in.file.stroke )
-# --remove--      print( head( stroke ))
-# --remove--  print( config$files$in.file.dmt2 )
-# --remove--      print( head( dmt2   ))
-
-
     # read the fitting parameters for the Burnett IER functions for all CODs and age classes
     rr <- read.csv( file = config$files$in.file.rr )
-# --remove--print( config$files$in.file.rr )                                        # --remove--
-# --remove--    print( head( rr   ))
 
     copd.med <- rr$MED[     ( rr$COD == 'COPD' ) & ( rr$AGE == 99 ) ]
     copd.lo  <- rr$X95CL_L[ ( rr$COD == 'COPD' ) & ( rr$AGE == 99 ) ]
@@ -124,45 +108,47 @@ health.impact <- function(
     dt2.lo  <- rr$X95CL_L[ ( rr$COD == 'DT2' ) & ( rr$AGE == 99 ) ]
     dt2.hi  <- rr$X95CL_H[ ( rr$COD == 'DT2' ) & ( rr$AGE == 99 ) ]
 
-# --remove--    print( copd.med )      #--remove--
-# --remove--    print( copd.lo  )
-# --remove--    print( copd.hi  )
-# --remove--    print( lri.med  )
-# --remove--    print( lri.lo   )
-# --remove--    print( lri.hi   )
-# --remove--    print( lc.med   )
-# --remove--    print( lc.lo    )
-# --remove--    print( lc.hi    )
-# --remove--    print( dt2.med  )
-# --remove--    print( dt2.lo   )
-# --remove--    print( dt2.hi   )
-
     # extract the appropriate RR parameters for each COD and assign to each variable - for easier tracking.
     ihd.med <- matrix( nrow = 4, data = rr$MED[     rr$COD == 'IHD' & rr$AGE %in% config$model$AGE_GRP ] )
     ihd.lo  <- matrix( nrow = 4, data = rr$X95CL_L[ rr$COD == 'IHD' & rr$AGE %in% config$model$AGE_GRP ] )
     ihd.hi  <- matrix( nrow = 4, data = rr$X95CL_H[ rr$COD == 'IHD' & rr$AGE %in% config$model$AGE_GRP ] )
 
-# --remove--  print( ihd.med )            # --remove--
-# --remove--  print( ihd.lo )
-# --remove--  print( ihd.hi )
-
     stroke.med <- matrix( nrow = 4, data = rr$MED[     rr$COD == 'STROKE' & rr$AGE %in% config$model$AGE_GRP ] )
     stroke.lo  <- matrix( nrow = 4, data = rr$X95CL_L[ rr$COD == 'STROKE' & rr$AGE %in% config$model$AGE_GRP ] )
     stroke.hi  <- matrix( nrow = 4, data = rr$X95CL_H[ rr$COD == 'STROKE' & rr$AGE %in% config$model$AGE_GRP ] )
 
-# --remove--  print( stroke.med )            # --remove--
-# --remove--  print( stroke.lo )
-# --remove--  print( stroke.hi )
 
     # Ozone RRs with log-lin ER function
     beta_rr_copd_tu  <- c( 1.14, 1.08, 1.21 ) / 10        # new TURNER!, for new exposure metric annual mean of daily 8h max!
     beta_rr_copd_gbd <- c( 1.06, 1.05, 1.10 ) / 10        # new GBD2017!, for new exposure metric 6-month mean of daily 8h max!
 
-# --remove--  print( beta_rr_copd_tu  )
-# --remove--  print( beta_rr_copd_gbd )
 
+    # *** ask Rita the file: country_mask_0.5x0.5_v3.sav
 
+    # high resolution lon lat dimensions
+    img     <- ncol( hrcntrcode )
+    jmg     <- nrow( hrcntrcode )
+    scale   <- img / ( xmax( hrcntrcode ) - xmin( hrcntrcode ) )
+# --not-used--    hr_lats <- ( ( ( 0:( ( ymax( hrcntrcode ) - ymin( hrcntrcode ) ) * scale - 1 ) ) + 0.5 ) / scale + ymin( hrcntrcode ) )
+# --not-used--    hr_lons <- ( ( ( 0:( ( xmax( hrcntrcode ) - xmin( hrcntrcode ) ) * scale - 1 ) ) + 0.5 ) / scale + xmin( hrcntrcode ) )
 
+    # med resolution lon lat dimensions
+    imed    <- 720   # img / 4 ?
+    jmed    <- 360   # jmg / 4 ?
+    scale   <- img / ( xmax( hrcntrcode ) - xmin( hrcntrcode ) )
+    mr_lats <- ( ( ( 0:( ( ymax( hrcntrcode ) - ymin( hrcntrcode ) ) * scale - 1 ) ) + 0.5 ) / scale + ymin( hrcntrcode ) )
+    mr_lons <- ( ( ( 0:( ( xmax( hrcntrcode ) - xmin( hrcntrcode ) ) * scale - 1 ) ) + 0.5 ) / scale + xmin( hrcntrcode ) )
+
+    # lon-lat arrays
+    hr_grid_tot  <- matrix( 0, nrow = jmg, ncol = img )
+    hr_grid_tot1 <- matrix( 0, nrow = jmg, ncol = img )    # intermediate placeholders needed when interpolating
+    hr_grid_tot2 <- matrix( 0, nrow = jmg, ncol = img )    # intermediate placeholders needed when interpolating
+    scenpopmask  <- hr_grid_tot
+    scenpop      <- hr_grid_tot
+    iminlat      <- 273                                    # index where hr_lats eq min lat in SSP hr grid
+    imaxlat      <- 1389                                   # index where hr_lats eq max lat in SSP hr grid
+
+    # layers containing the AFs, depends on PM fields
 
 
 
