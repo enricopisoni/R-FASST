2ddc7f8619f76f6c09f5dc4abddfdefce1730a7f
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-17 16:52:01 +0200

    directories-arrange: batch files for testing commented

--- R/test.bat
+++ R/test.bat
@@ -1,3 +1,9 @@
 @echo off
 
+rem  Script file to simply test the model.
+rem  We call the R script with dummies: project name (Project), model name (Model)
+rem  and model version (1.0); the working directory will be '../Files/testing' and
+rem  in the latter directory the script will use the configuration
+rem  file './testing-config.json'.
+rem  
 call  fasst-4-sherpa.bat  Project  Model  1.0  ../Files/testing  ./testing-config.json
