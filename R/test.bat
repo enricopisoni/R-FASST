7b1c2d745888634f831b41cbe8039bb48081bbfd
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-17 15:53:36 +0200

    directories-arrange: R files moved in subdirectory

---
+++ R/test.bat
@@ -0,0 +1,3 @@
+@echo off
+
+call  fasst-4-sherpa.bat  Project  Model  1.0  ../Files/testing  ./testing-config.json
---
+++ R/test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./fasst-4-sherpa.sh  Project  Model  1.0  ../Files/testing  ./testing-config.json
