f0bdd396a50b50a65eb0c425e13b2a976ef0a30d
Alessandro Ernesto Mascherpa <alessandro.mascherpa@piksel.com>
2020-04-24 10:45:49 +0200

    development: rrate function simplified

--- R/rrate.R
+++ R/rrate.R
@@ -11,17 +11,12 @@
 #'
 rrate <- function( C, PM )
 {
-	# Initialize RR with same dimensions as PM
-	RR <- numeric( length( PM ) )
-
-	# elements less than than C[4]
-	jj <- PM >= C[ 4 ]
 	# Other grid cells: RR as a function of 4
 	# C[i] function parameter
-	RR[ jj ] <- 1 + C[1] * ( 1 - exp( -C[2] * ( PM[ jj ] - C[4] ) ^ C[3] ) )
+	RR                 <- 1 + C[1] * ( 1 - exp( -C[2] * ( PM - C[4] ) ^ C[3] ) )
 
 	# grid cells where PM < C[4] get RR=1
-	RR[ ! jj ]  <- 1
+	RR[ PM < C[ 4 ] ]  <- 1
 
 	# return result
 	RR
